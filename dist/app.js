9e115e9bfea9081377cfe03e59e566edd3ac55e8
aivaras23 <aivaraspal1@gmail.com>
2024-04-20 17:28:05 +0300

    update2

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,9 @@ const receptoDur = document.getElementById('gTrukme');
 const receptoDesc = document.getElementById('aprasymas');
 const submitBtn = document.getElementById('submitBtn');
 const recipeEditor = document.getElementById('recipeEditor');
+const showAllRecipesBtn = document.getElementById('showAllRecipes');
+const updateBtn = document.getElementById('updateBtn');
+const deleteBtn = document.getElementById('deleteBtn');
 const recipeData = [];
 submitBtn.onclick = () => {
     if (receptoName.value === '' || receptoName.value === '' || receptoDesc.value === '') {
@@ -53,17 +56,85 @@ submitBtn.onclick = () => {
     newRecipeButtons.classList.add('recipe-buttons');
     const newRecipeUpdateBtn = document.createElement('button');
     newRecipeUpdateBtn.innerHTML = 'Atnaujinti';
+    newRecipeUpdateBtn.id = 'updateBtn';
     const newRecipeDeleteBtn = document.createElement('button');
     newRecipeDeleteBtn.innerHTML = 'Ištrinti';
+    newRecipeDeleteBtn.id = 'deleteBtn';
     newRecipeButtons.appendChild(newRecipeUpdateBtn);
     newRecipeButtons.appendChild(newRecipeDeleteBtn);
     newRecipeDiv.appendChild(newRecipeName);
     newRecipeDiv.appendChild(newRecipeDur);
     newRecipeDiv.appendChild(newRecipeDesc);
-    newRecipeDiv.appendChild(newRecipeButtons);
     const receptoInfoCon = document.querySelector('.recipe-wrapper');
     receptoInfoCon.appendChild(newRecipeDiv);
+    receptoInfoCon.appendChild(newRecipeButtons);
 };
+const loadData = () => {
+    fetch('https://recipedb-fc213-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((res) => {
+        return res.json();
+    })
+        .then((data) => {
+        const receptoInfoCon = document.querySelector('.recipe-wrapper');
+        receptoInfoCon.innerHTML = '';
+        Object.keys(data).forEach((key) => {
+            const recipe = data[key];
+            const newRecipeDiv = document.createElement('div');
+            newRecipeDiv.classList.add('recipe-info-con');
+            newRecipeDiv.setAttribute('contentEditable', 'true');
+            const newRecipeName = document.createElement('div');
+            const newRecipeNameSpan = document.createElement('span');
+            newRecipeName.classList.add('recipe-name');
+            newRecipeNameSpan.innerHTML = recipe.recipeName;
+            newRecipeName.appendChild(newRecipeNameSpan);
+            const newRecipeDur = document.createElement('div');
+            const newRecipeDurSpan = document.createElement('span');
+            newRecipeDur.classList.add('recipe-duration');
+            newRecipeDurSpan.innerHTML = recipe.recipeDuration;
+            newRecipeDur.appendChild(newRecipeDurSpan);
+            const newRecipeDesc = document.createElement('div');
+            const newRecipePara = document.createElement('p');
+            newRecipeDesc.classList.add('recipe-desc');
+            newRecipePara.innerHTML = recipe.recipeDescription;
+            newRecipeDesc.appendChild(newRecipePara);
+            const newRecipeButtons = document.createElement('div');
+            newRecipeButtons.classList.add('recipe-buttons');
+            const newRecipeUpdateBtn = document.createElement('button');
+            newRecipeUpdateBtn.innerHTML = 'Atnaujinti';
+            const newRecipeDeleteBtn = document.createElement('button');
+            newRecipeDeleteBtn.innerHTML = 'Ištrinti';
+            newRecipeButtons.appendChild(newRecipeUpdateBtn);
+            newRecipeButtons.appendChild(newRecipeDeleteBtn);
+            newRecipeDiv.appendChild(newRecipeName);
+            newRecipeDiv.appendChild(newRecipeDur);
+            newRecipeDiv.appendChild(newRecipeDesc);
+            receptoInfoCon.appendChild(newRecipeDiv);
+            receptoInfoCon.appendChild(newRecipeButtons);
+            newRecipeDeleteBtn.addEventListener('click', () => {
+                deleteData(key);
+            });
+        });
+    });
+};
+showAllRecipesBtn.onclick = loadData;
+function deleteData(key) {
+    fetch(`https://recipedb-fc213-default-rtdb.europe-west1.firebasedatabase.app/recipes/${key}.json`, {
+        method: 'DELETE',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((data) => {
+        loadData();
+    });
+}
 // recipeEditor.onclick = () => {
 //     if(recipeEditor.contentEditable === 'true') {
 //         recipeEditor.contentEditable = 'false';
--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,12 @@ const submitBtn = <HTMLButtonElement> document.getElementById('submitBtn');
 
 const recipeEditor = <HTMLElement> document.getElementById('recipeEditor');
 
+const showAllRecipesBtn = <HTMLButtonElement> document.getElementById('showAllRecipes');
+
+
+const updateBtn = <HTMLButtonElement> document.getElementById('updateBtn');
+const deleteBtn = <HTMLButtonElement> document.getElementById('deleteBtn');
+
 
 const recipeData:Recipes[] = [];
 
@@ -38,7 +44,7 @@ submitBtn.onclick = () => {
         console.log(data);
     })
 
-   
+        
         const newRecipeDiv = document.createElement('div');
         newRecipeDiv.classList.add('recipe-info-con');
         newRecipeDiv.setAttribute('contentEditable', 'true');
@@ -66,20 +72,104 @@ submitBtn.onclick = () => {
         newRecipeButtons.classList.add('recipe-buttons');
         const newRecipeUpdateBtn = document.createElement('button');
         newRecipeUpdateBtn.innerHTML = 'Atnaujinti';
+        newRecipeUpdateBtn.id = 'updateBtn';
         const newRecipeDeleteBtn = document.createElement('button');
         newRecipeDeleteBtn.innerHTML = 'Ištrinti';
+        newRecipeDeleteBtn.id = 'deleteBtn';
         newRecipeButtons.appendChild(newRecipeUpdateBtn);
         newRecipeButtons.appendChild(newRecipeDeleteBtn);
 
         newRecipeDiv.appendChild(newRecipeName);
         newRecipeDiv.appendChild(newRecipeDur);
         newRecipeDiv.appendChild(newRecipeDesc);
-        newRecipeDiv.appendChild(newRecipeButtons);
 
         const receptoInfoCon = <HTMLElement> document.querySelector('.recipe-wrapper');
         receptoInfoCon.appendChild(newRecipeDiv);
+        receptoInfoCon.appendChild(newRecipeButtons);
 
 }
+
+
+const loadData = () => {
+        fetch('https://recipedb-fc213-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', {
+        method: 'GET',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        }
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((data: { [key: string]: Recipes}) => {
+        const receptoInfoCon = <HTMLElement> document.querySelector('.recipe-wrapper');
+        receptoInfoCon.innerHTML = '';
+
+        Object.keys(data).forEach((key)=> {
+            const recipe = data[key];
+
+            const newRecipeDiv = document.createElement('div');
+            newRecipeDiv.classList.add('recipe-info-con');
+            newRecipeDiv.setAttribute('contentEditable', 'true');
+
+            const newRecipeName = document.createElement('div');
+            const newRecipeNameSpan = document.createElement('span');
+            newRecipeName.classList.add('recipe-name');
+            newRecipeNameSpan.innerHTML = recipe.recipeName;
+            newRecipeName.appendChild(newRecipeNameSpan);
+
+            const newRecipeDur = document.createElement('div');
+            const newRecipeDurSpan = document.createElement('span');
+            newRecipeDur.classList.add('recipe-duration');
+            newRecipeDurSpan.innerHTML = recipe.recipeDuration;
+            newRecipeDur.appendChild(newRecipeDurSpan);
+
+            const newRecipeDesc = document.createElement('div');
+            const newRecipePara = document.createElement('p');
+            newRecipeDesc.classList.add('recipe-desc');
+            newRecipePara.innerHTML = recipe.recipeDescription;
+            newRecipeDesc.appendChild(newRecipePara);
+
+            const newRecipeButtons = document.createElement('div');
+            newRecipeButtons.classList.add('recipe-buttons');
+            const newRecipeUpdateBtn = document.createElement('button');
+            newRecipeUpdateBtn.innerHTML = 'Atnaujinti';
+            const newRecipeDeleteBtn = document.createElement('button');
+            newRecipeDeleteBtn.innerHTML = 'Ištrinti';
+            newRecipeButtons.appendChild(newRecipeUpdateBtn);
+            newRecipeButtons.appendChild(newRecipeDeleteBtn);
+
+            newRecipeDiv.appendChild(newRecipeName);
+            newRecipeDiv.appendChild(newRecipeDur);
+            newRecipeDiv.appendChild(newRecipeDesc);
+
+            receptoInfoCon.appendChild(newRecipeDiv);
+            receptoInfoCon.appendChild(newRecipeButtons);
+
+            newRecipeDeleteBtn.addEventListener('click', () => {
+                deleteData(key); 
+            });
+        })
+    })
+}
+
+showAllRecipesBtn.onclick = loadData
+
+function deleteData(key: string) {
+    fetch(`https://recipedb-fc213-default-rtdb.europe-west1.firebasedatabase.app/recipes/${key}.json`, {
+        method: 'DELETE',
+        headers:{
+            'Accept':'application/json',
+            'Content-Type':'application/json'
+        }
+    })
+    .then((data) => {
+        loadData();
+    })
+}
+
+
+
 
 // recipeEditor.onclick = () => {
 //     if(recipeEditor.contentEditable === 'true') {
@@ -105,3 +195,5 @@ document.addEventListener('click', (event: MouseEvent) => {
         recipeEditor.contentEditable = 'false';
     }
 })
+
+
